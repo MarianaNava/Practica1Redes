8e798227dc9d5bba63930c433f66a62d8fa7fd54
MarianaNava <mariana.nava@ciencias.unam.mx>
2024-09-28 15:04:37 -0600

    inicializacion de palabra

--- servidor.py
+++ servidor.py
@@ -79,8 +79,27 @@ estados = [
     """
 ]
 
+def transforma_a_lineas(palabra):
+    cadena = ""
+    for letra in palabra:
+        cadena += "_ "
+    return cadena[:-1]
+
 # seleccionamos una palabra aleatoria del banco de palabras
 palabra_actual = banco_de_palabras[random.randint(0,len(banco_de_palabras))]
+errores = 0
+progreso_palabra = transforma_a_lineas(palabra_actual)
+
+
+
+
+
+#mensaje = dict(progreso = La palabra adivinada hasta el momento.
+#    estado = estados[errores]
+#    juego = "en progreso"
+#    siguiente_jugador = El jugador con el tiro siguiente.)
+
+
 
 # Recepción de mensajes del cliente y actualización de la partida, con manejo de errores.
 
