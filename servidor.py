c2e2d0f5d38a7ce47df8c49df685cc815fbb8ecc
MarianaNava <mariana.nava@ciencias.unam.mx>
2024-09-28 14:43:35 -0600

    constantes y primer diseño de mensaje

--- servidor.py
+++ servidor.py
@@ -1,5 +1,6 @@
 from socket import *
 import json
+import random
 
 # Validación de sesión y envío de cookies.
 
@@ -7,7 +8,79 @@ import json
 
 # Lógica del juego Ahorcado.
 
-# Código ...
+banco_de_palabras = ["cielo", "burbuja", "caracol", "piano", "aspiradora", "mariposa",
+                     "pavo", "caramelo", "banjo", "flor", "perfume", "llama",
+                     "bicicleta", "estrella", "guitarra", "microscopio", "rompecabezas",
+                     "ballena", "submarino", "abeja", "joyero", "gatito", "pirata",
+                     "disco", "tornado", "oboe", "uvas", "lentes", "gacela",
+                     "brillantina", "equipaje", "cacahuates", "oruga", "colcha",
+                     "maracas", "bolsillo", "violonchelo", "reloj", "catedral", "tijera",
+                     "tortuga", "nuez", "pantalla", "cometa", "lavanda", "telescopio",
+                     "pimienta", "cebra", "bolsa", "cafetera", "vela", "canguro", "fuego",
+                     "muelle", "cereza", "taller", "cascada", "lupa", "trompeta", "cactus",
+                     "cuadro", "sombra", "pelo", "cena", "bote", "lago", "cuna"]
+
+estados = [
+    """
+    _____
+    |   |
+    |
+    |
+    |
+    |
+    """,
+    """
+    _____
+    |   |
+    |   O
+    |
+    |
+    |
+    """,
+    """
+    _____
+    |   |
+    |   O
+    |   |
+    |
+    |
+    """,
+    """
+    _____
+    |   |
+    |   O
+    |  /|
+    |
+    |
+    """,
+    """
+    _____
+    |   |
+    |   O
+    |  /|\\
+    |
+    |
+    """,
+    """
+    _____
+    |   |
+    |   O
+    |  /|\\
+    |  /
+    |
+    """,
+    """
+    _____
+    |   |
+    |   O
+    |  /|\\
+    |  / \\
+    |
+    """
+]
+
+# seleccionamos una palabra aleatoria del banco de palabras
+palabra_actual = banco_de_palabras[random.randint(0,len(banco_de_palabras))]
 
 # Recepción de mensajes del cliente y actualización de la partida, con manejo de errores.
 
