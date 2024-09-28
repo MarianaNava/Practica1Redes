83c25e117533781e3a15705226243916c4ed1be6
MarianaNava <mariana.nava@ciencias.unam.mx>
2024-09-28 13:36:57 -0600

    mini estructura para reducir conflictos

--- servidor.py
+++ servidor.py
@@ -1,2 +1,18 @@
 from socket import *
 import json
+
+# Validación de sesión y envío de cookies.
+
+# Código ...
+
+# Lógica del juego Ahorcado.
+
+# Código ...
+
+# Recepción de mensajes del cliente y actualización de la partida, con manejo de errores.
+
+# Código ...
+
+# Persistencia de la partida.
+
+# Código ...
